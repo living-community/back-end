ca72733710ed8d870422fb89190c4037798fdea1
CreatePath <hary0427@naver.com>
2024-01-30 23:23:32 +0900

    fix: remove calling auth functions

--- router/post.js
+++ router/post.js
@@ -11,25 +11,25 @@ const postController = container.get("PostController");
 
 router.route("/")
     .get(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => postController.handleGetPostList(req, res)
     )
     .post(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => postController.handleCreatePost(req, res),
     );
 
 router.route("/:id")
     .get(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => postController.handleGetPost(req, res)
     )
     .patch(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => postController.handleUpdatePost(req, res)
     )
     .delete(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => postController.handleDeletePost(req, res)
     );
 
--- router/user.js
+++ router/user.js
@@ -8,11 +8,11 @@ const authController = container.get("AuthController");
 router.route("/:nickname")
     .get((req, res) => userController.handleGetUser(req, res))
     .patch(
-        (req, res, next) => authController.authToken(req, res, next), 
+        // (req, res, next) => authController.authToken(req, res, next), 
         (req, res) => userController.handleUpdateUser(req, res)
     )
     .delete(
-        (req, res, next) => authController.authToken(req, res, next),
+        // (req, res, next) => authController.authToken(req, res, next),
         (req, res) => userController.handleDeleteUser(req, res)
     );
 
